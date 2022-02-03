e7efd0d2cba2a69d605a283692ae4dda909bca42
Erick Mayorga <erick.mayorga@epn.edu.ec>
2022-02-03 15:53:49 -0500

    Firebase Authentication - Lab 07

--- app/src/main/java/com/mayorgaerick/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/mayorgaerick/cazarpatos/LoginActivity.kt
@@ -4,9 +4,14 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.mayorgaerick.cazarpatos.FileManagers.FileHandler
 import com.mayorgaerick.cazarpatos.FileManagers.SharedPreferencesManager
 
@@ -18,6 +23,7 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var buttonLogin: Button
     private lateinit var buttonNewUser:Button
     private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +34,10 @@ class LoginActivity : AppCompatActivity() {
         buttonLogin = findViewById(R.id.buttonLogin)
         buttonNewUser = findViewById(R.id.buttonNewUser)
         checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
 
         //File Managers
         manejadorArchivo = SharedPreferencesManager(this)
@@ -49,9 +59,11 @@ class LoginActivity : AppCompatActivity() {
             GuardarDatosEnPreferencias()
 
             //Si pasa validación de datos requeridos, ir a pantalla principal
-            val intencion = Intent(this, MainActivity::class.java)
-            intencion.putExtra(EXTRA_LOGIN, email)
-            startActivity(intencion)
+            //val intencion = Intent(this, MainActivity::class.java)
+            //intencion.putExtra(EXTRA_LOGIN, email)
+            //startActivity(intencion)
+
+            AutenticarUsuario(email, clave)
         }
         buttonNewUser.setOnClickListener{
 
@@ -60,21 +72,40 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer.start()
     }
 
+    private fun AutenticarUsuario(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(EXTRA_LOGIN, "signInWithEmail:success")
+                    //Si pasa validación de datos requeridos, ir a pantalla principal
+                    val intencion = Intent(this, MainActivity::class.java)
+                    intencion.putExtra(EXTRA_LOGIN, auth.currentUser!!.email)
+                    startActivity(intencion)
+                    //finish()
+                } else {
+                    Log.w(EXTRA_LOGIN, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
     private fun ValidarDatosRequeridos():Boolean{
         val email = editTextEmail.text.toString()
         val clave = editTextPassword.text.toString()
         if (email.isEmpty()) {
-            editTextEmail.setError("El email es obligatorio")
+            editTextEmail.error = "El email es obligatorio"
             editTextEmail.requestFocus()
             return false
         }
         if (clave.isEmpty()) {
-            editTextPassword.setError("La clave es obligatoria")
+            editTextPassword.error = "La clave es obligatoria"
             editTextPassword.requestFocus()
             return false
         }
         if (clave.length < 3) {
-            editTextPassword.setError("La clave debe tener al menos 3 caracteres")
+            editTextPassword.error = "La clave debe tener al menos 3 caracteres"
             editTextPassword.requestFocus()
             return false
         }
